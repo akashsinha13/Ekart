1e21a38f8c015816991b5f30553ae6d9f69ab048
Akash Sinha <sinhaakashak@gmail.com>
2021-10-17 21:05:22 +0530

    initial project setup

---
+++ src/main/java/com/ekart/EkartApplication.java
@@ -0,0 +1,14 @@
+package com.ekart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekart/EkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

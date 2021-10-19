0d07a823c52d03f49dd11b0c7446de0693d80c64
Akash Sinha <sinhaakashak@gmail.com>
2021-10-19 18:36:58 +0530

    added swagger

--- src/main/java/com/ekart/EkartApplication.java
+++ src/main/java/com/ekart/EkartApplication.java
@@ -2,7 +2,9 @@ package com.ekart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class EkartApplication {
 

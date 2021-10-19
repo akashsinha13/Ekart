cb69b02a3e7fd96ff4f82a38f9b6f2c7a93f662a
Akash Sinha <sinhaakashak@gmail.com>
2021-10-19 20:33:43 +0530

    added product type

--- src/main/java/com/ekart/model/Product.java
+++ src/main/java/com/ekart/model/Product.java
@@ -1,10 +1,19 @@
 package com.ekart.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
 public class Product {
     private Long id;
 
@@ -22,6 +31,8 @@ public class Product {
 
     private ProductColor color;
 
+    private ProductType type;
+
     private ProductBrand brand;
 
     private Integer likes;
@@ -35,4 +46,8 @@ public class Product {
     private LocalDate lastModifiedDate;
 
     private Map<String, List<String>> info;
+
+    private byte[] thumbnailImage;
+
+    private List<Byte[]> images;
 }
---
+++ src/main/java/com/ekart/model/ProductType.java
@@ -0,0 +1,9 @@
+package com.ekart.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductType {
+    public String type = null;
+    public List<String> subTypes = new ArrayList<>();
+}
